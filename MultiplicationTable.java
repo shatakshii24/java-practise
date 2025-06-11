f1b5b991707998852ef5c0711ece2748f28642cf
shatakshii24 <shatakshimarathe246@gmail.com>
2025-06-11 12:19:16 +0530

    Add files via upload

---
+++ MultiplicationTable.java
@@ -0,0 +1,8 @@
+public class MultiplicationTable{
+    public static void main(String[] args){
+    int number = 49;
+    for(int i = 1; i<=10 ; i++){
+        System.out.println(number + " X " + i + " = " + (number*i));
+    }
+}
+}
